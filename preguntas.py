2faba74e6bf8e0ec159e5abdfc20ee5971d78b05
yinier <53497366+yinier@users.noreply.github.com>
2022-05-22 15:53:18 -0500

    SUBMISSION 3 Complete

--- preguntas.py
+++ preguntas.py
@@ -146,7 +146,7 @@ def pregunta_04():
     """
 
     # Importe mean_squared_error
-    from ____ import ____
+    from sklearn.metrics import mean_squared_error as mse
 
     # Obtenga el pipeline optimo de la pregunta 3.
     gridSearchCV = pregunta_03()
@@ -155,20 +155,20 @@ def pregunta_04():
     X_train, X_test, y_train, y_test = pregunta_02()
 
     # Evalúe el modelo con los conjuntos de entrenamiento y prueba.
-    y_train_pred = ____.____(____)
-    y_test_pred = ____.____(____)
+    
+    y_train_pred = gridSearchCV.predict(X_train) 
+    y_test_pred = gridSearchCV.predict(X_test) 
 
     # Compute el error cuadratico medio de entrenamiento y prueba. Redondee los
     # valores a dos decimales.
 
-    mse_train = ____(
-        _____,
-        _____,
+    mse_train = mse(  
+        y_train_pred,  
+        y_train,  
     ).round(2)
-
-    mse_test = ____(
-        _____,
-        _____,
+    mse_test = mse(  
+        y_test_pred,  
+        y_test,  
     ).round(2)
 
     # Retorne el error cuadrático medio para entrenamiento y prueba
